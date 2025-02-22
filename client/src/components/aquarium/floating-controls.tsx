af0b00b3194737bce424793c3e53f89d46fc4253
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2025-02-22 09:38:59 +0100

    fix: floating controls

--- client/src/components/aquarium/floating-controls.tsx
+++ client/src/components/aquarium/floating-controls.tsx
@@ -1,6 +1,8 @@
 import Button from "@/components/ui/button";
+import { useState } from "react";
 
 export default function FloatingControls() {
+  const [isOpen, setIsOpen] = useState(false);
   const controls = [
     "/textures/icons/Cup01.svg",
     "/textures/icons/Basket.svg",
@@ -10,21 +12,36 @@ export default function FloatingControls() {
     "/textures/icons/Arrow02Up.svg",
   ];
 
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
   return (
     <div className="flex items-center gap-2 absolute bottom-4 right-4 md:bottom-8 md:right-8">
-      <div className="h-16 md:h-20 flex items-center justify-center bg-cyan-500 rounded-full py-2 px-5 shadow-[0px_5px_0px_rgba(1,147,201,1)] cursor-pointer">
+      <div
+        className={`h-16 flex items-center justify-center bg-cyan-500 rounded-full py-2 px-5 shadow-[0px_5px_0px_rgba(1,147,201,1)] cursor-pointer transition-all duration-300 ${
+          isOpen
+            ? "opacity-100 scale-100"
+            : "opacity-0 scale-90 pointer-events-none"
+        }`}
+      >
         {controls.map((icon, index) => (
-          <button
+          <div
             key={index}
-            className="w-6 md:w-8 h-6 md:h-8 flex items-center justify-center mx-1 md:mx-2 transition-transform duration-200 hover:scale-110 active:scale-95 active:shadow-inner"
+            onClick={handleClick}
+            className="w-6 md:w-8 h-6 md:h-8 flex items-center justify-center mx-1 md:mx-2 transition-transform duration-200 hover:scale-110 active:scale-95 active:shadow-inner cursor-pointer"
           >
             <img src={icon} alt={`${icon}-icon`} className="object-contain" />
-          </button>
+          </div>
         ))}
       </div>
 
       <div className="h-16 md:h-20 flex items-center">
-        <Button color="teal" className="shrink-0">
+        <Button
+          color="teal"
+          className="shrink-0"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <img
             src="/textures/icons/Tiles.svg"
             alt="Icon"
