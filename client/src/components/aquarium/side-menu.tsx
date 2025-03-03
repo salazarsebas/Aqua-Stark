d503a04f0ed98f0849f6c6bc7477faad7acab787
Josue19-08 <josuemarin2009@hotmail.com>
2025-03-03 16:00:46 -0600

    fix: modify side menu

--- client/src/components/aquarium/side-menu.tsx
+++ client/src/components/aquarium/side-menu.tsx
@@ -1,25 +1,12 @@
-import Button from "@/components/ui/button"; 
+import Button from "@/components/ui/button";
 
 export default function SideMenu() {
   return (
     <div className="absolute right-4 top-20 flex flex-col space-y-2 md:right-10 md:top-36">
-     
-<Button color="teal" iconSrc="/textures/icons/Chest02.svg" className="md:w-12 md:h-12 w-6 h-6 flex items-center justify-center p-1" />
-<Button
-  color="yellow"
-  className="md:w-12 md:h-12 w-6 h-6 flex items-center justify-center p-1"
->
-  <div className="rounded-full bg-white flex items-center justify-center md:w-6 md:h-6 w-5 h-5">
-    <img src="/textures/icons/StarOrange.svg" alt="icon" className="md:w-5 md:h-5 w-4 h-4" />
-  </div>
-</Button>
-<Button color="red" iconSrc="/textures/icons/Chat.svg" className="md:w-12 md:h-12 w-6 h-6 flex items-center justify-center p-1" />
-<Button color="green" iconSrc="/textures/icons/Book01.svg" className="md:w-12 md:h-12 w-6 h-6 flex items-center justify-center p-1" />
-        
-          
-          
-           </div>
-    
-   
+      <Button color="teal" size="medium" iconSrc="/textures/icons/Chest02.svg" />
+      <Button color="yellow" size="medium" iconSrc="/textures/icons/Chat.svg" />
+      <Button color="red" size="medium" iconSrc="/textures/icons/Chat.svg" /> 
+      <Button color="green" size="medium" iconSrc="/textures/icons/Book01.svg" />
+    </div>
   );
 }
