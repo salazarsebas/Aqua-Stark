880e94481eb7aa2926dfa2c78b2d644d4b3d78c3
Josue19-08 <josuemarin2009@hotmail.com>
2025-03-04 21:37:35 -0600

    feat: modify fsh-counter

--- client/src/components/ui/fish-counter.tsx
+++ client/src/components/ui/fish-counter.tsx
@@ -1,10 +1,11 @@
 import { mockAquariumStats } from "@/data/mock-data";
+import { FishIcon } from "@/components/icons/FishIcon";
 
 export default function FishCounter() {
   return (
-    <div className="relative  w-16 h-16">
-      <img src="/icons/fish.png" alt="Fish Count" width={36} height={36} className="w-full h-full" />
-      <span className="absolute top-[50%] left-[50%] transform translate-y-[-50%] translate-x-[-50%] w-full h-full flex items-center justify-center   text-white font-bold text-base">
+    <div className="relative w-20 h-20">
+      <FishIcon className="w-14 h-14 text-white" />
+      <span className="absolute top-[50%] left-[50%] transform translate-y-[-50%] translate-x-[-50%] w-full h-full flex items-center justify-center text-white font-bold text-lg">
         {mockAquariumStats.currentFish} / {mockAquariumStats.maxFish}
       </span>
     </div>
