1a8ab62c465ca11ef66ddc2b6f3b03d5d86b4813
Elite-tch <elite6461@gmail.com>
2025-03-23 18:37:10 +0100

    Breeding Laboratory Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/landing-page"
 import GamePage from "./pages/game-page"
-//import StorePage from "./pages/storage-page"
-import BreedingLaboratory from "./pages/breeding-laboratory"
+import StorePage from "./pages/storage-page"
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/game" element={<GamePage />} />
-      {/*<Route path="/store" element={<StorePage />} />*/}
-      <Route path="/store" element={<BreedingLaboratory />} />
+      <Route path="/store" element={<StorePage />} />
+     
     </Routes>
   )
 }
