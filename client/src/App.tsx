83daf912c35c2d1672c81daa3313bf665aa7d2fe
Elite-tch <elite6461@gmail.com>
2025-03-25 07:47:01 +0100

    flex adjustment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing-page";
 import GamePage from "./pages/game-page";
-//import StorePage from "./pages/storage-page";
+import StorePage from "./pages/storage-page";
 import AchievementsPage from "./pages/achievements-page";
 import BreedingLaboratory from "./pages/breeding-laboratory";
 function App() {
@@ -9,8 +9,8 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/game" element={<GamePage />} />
-     {/*} <Route path="/store" element={<StorePage />} />*/}
-     <Route path="/store" element={<BreedingLaboratory />} />
+     <Route path="/store" element={<StorePage />} />
+     <Route path="/breedingLaboratory" element={<BreedingLaboratory />} />
       <Route path="/achievements" element={<AchievementsPage />} />
 
     </Routes>
