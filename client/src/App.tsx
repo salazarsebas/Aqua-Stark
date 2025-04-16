762160809d5aff7e069a9de05bd85b344f9afa28
Josue19-08 <josuemarin2009@hotmail.com>
2025-04-15 20:58:44 -0600

    refactor: remove old footer component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,15 @@
-
-import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/landing-page";
-import GamePage from "./pages/game-page";
-import StorePage from "./pages/storage-page";
-import AchievementsPage from "./pages/achievements-page";
-import HelpCenter from "./pages/help-center";
-import BreadingLaboratory from "./pages/breeding-laboratory";
-import EventsCalendar from "./pages/events-calendar";
-import TradingMarketPage from "./pages/trading-market";
-import CommunityPage from "./pages/community-page";
-import MyProfile from "@/pages/my-profile";
-import EncyclopediaPage from "./pages/encyclopledia";
-
-
+import { Routes, Route } from "react-router-dom"
+import LandingPage from "./pages/landing-page"
+import GamePage from "./pages/game-page"
+import StorePage from "./pages/storage-page"
+import AchievementsPage from "./pages/achievements-page"
+import HelpCenter from "./pages/help-center"
+import BreadingLaboratory from "./pages/breeding-laboratory"
+import EventsCalendar from "./pages/events-calendar"
+import TradingMarketPage from "./pages/trading-market"
+import CommunityPage from "./pages/community-page"
+import MyProfile from "./pages/my-profile"
+import EncyclopediaPage from "./pages/encyclopedia"
 
 function App() {
   return (
@@ -20,16 +17,17 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/game" element={<GamePage />} />
       <Route path="/store" element={<StorePage />} />
-      <Route path="/breeding-laboratory" element={<BreadingLaboratory/>} />
-      <Route path="/encyclopedia" element={<EncyclopediaPage/>} />
+      <Route path="/breeding-laboratory" element={<BreadingLaboratory />} />
+      <Route path="/encyclopedia" element={<EncyclopediaPage />} />
       <Route path="/achievements" element={<AchievementsPage />} />
       <Route path="/help-center" element={<HelpCenter />} />
-      <Route path="/events" element={<EventsCalendar/>} />
+      <Route path="/events" element={<EventsCalendar />} />
       <Route path="/trading-market" element={<TradingMarketPage />} />
       <Route path="/community" element={<CommunityPage />} />
-      <Route path="/my-profile" element={<MyProfile/>} />
+      <Route path="/my-profile" element={<MyProfile />} />
+      <Route path="/storage-page" element={<StorePage />} />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
